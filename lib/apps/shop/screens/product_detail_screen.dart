bee3d80dab838c80f4dd06269aeae0c1ef4d53ea
Claudio Massao Uyeno <claudio.massao@gmail.com>
2020-09-01 22:27:08 -0300

    product detail screen

--- lib/apps/shop/screens/product_detail_screen.dart
+++ lib/apps/shop/screens/product_detail_screen.dart
@@ -9,11 +9,45 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context).settings.arguments as String;
-    final selectedProduct = Provider.of<Products>(context, listen: false).getById(productId);
+    final selectedProduct =
+        Provider.of<Products>(context, listen: false).getById(productId);
 
     return Scaffold(
       appBar: AppBar(
         title: Text(selectedProduct.title),
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: 300,
+            width: double.infinity,
+            child: Image.network(
+              selectedProduct.imageUrl,
+              fit: BoxFit.fill,
+            ),
+          ),
+          SizedBox(height: 10),
+          Text(
+            'R\$ ${selectedProduct.price}',
+            style: TextStyle(
+              color: Theme.of(context).primaryColor,
+              fontSize: 20,
+            ),
+          ),
+          SizedBox(height: 10),
+          Container(
+            width: double.infinity,
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: Text(
+              selectedProduct.description,
+              textAlign: TextAlign.center,
+              softWrap: true,
+              style: TextStyle(
+                color: Colors.grey,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
