ff0aaeee89d4dce80d009868e5b3f5fc89dd2f9c
Claudio Massao Uyeno <claudio.massao@gmail.com>
2020-10-15 22:16:09 -0300

    authentication screen

--- lib/apps/shop/app_shop.dart
+++ lib/apps/shop/app_shop.dart
@@ -7,15 +7,18 @@ import './screens/cart_screen.dart';
 import './screens/orders_screen.dart';
 import './screens/user_products_screen.dart';
 import './screens/edit_product_screen.dart';
+import './screens/auth_screen.dart';
 import './providers/products.dart';
 import './providers/cart.dart';
 import './providers/orders.dart';
+import './providers/auth.dart';
 
 class AppShop extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
+        ChangeNotifierProvider(create: (ctx) => Auth()),
         ChangeNotifierProvider(create: (ctx) => Products()),
         ChangeNotifierProvider(create: (ctx) => Cart()),
         ChangeNotifierProvider(create: (ctx) => Orders())
@@ -27,13 +30,14 @@ class AppShop extends StatelessWidget {
           accentColor: Colors.yellowAccent,
           fontFamily: 'Lato',
         ),
-        home: ProductsOverviewScreen(),
+        home: AuthScreen(), //ProductsOverviewScreen(),
         routes: {
           ProductDetailScreen.route_name: (ctx) => ProductDetailScreen(),
           CartScreen.route_name: (ctx) => CartScreen(),
           OrderScreen.route_name: (ctx) => OrderScreen(),
           UserProductsScreen.route_name: (ctx) => UserProductsScreen(),
           EditProductScreen.rout_name: (ctx) => EditProductScreen(),
+          AuthScreen.routeName: (ctx) => AuthScreen(),
         },
       ),
     );
