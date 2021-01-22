a8a9dd5a958a3da70c11e04b6ef28bedbbd0458a
Claudio Massao Uyeno <claudio.massao@gmail.com>
2021-01-22 16:53:30 -0300

    animation using builtin AnimatedContainer

--- lib/apps/shop/screens/auth_screen.dart
+++ lib/apps/shop/screens/auth_screen.dart
@@ -111,14 +111,16 @@ class _AuthCardState extends State<AuthCard>
   @override
   void initState() {
     super.initState();
-    _animationController =
-        AnimationController(vsync: this, duration: Duration(milliseconds: 300));
-    _heightAnimation = Tween<Size>(
-            begin: Size(double.infinity, 260), end: Size(double.infinity, 320))
-        .animate(CurvedAnimation(
-            parent: _animationController, curve: Curves.linear));
 
-    _heightAnimation.addListener(() => setState(() {}));
+    //AnimatedContainer não precisa de controller e animation
+    // _animationController =
+    //     AnimationController(vsync: this, duration: Duration(milliseconds: 300));
+    // _heightAnimation = Tween<Size>(
+    //         begin: Size(double.infinity, 260), end: Size(double.infinity, 320))
+    //     .animate(CurvedAnimation(
+    //         parent: _animationController, curve: Curves.linear));
+
+    // _heightAnimation.addListener(() => setState(() {}));
   }
 
   @override
@@ -192,12 +194,12 @@ class _AuthCardState extends State<AuthCard>
       setState(() {
         _authMode = AuthMode.Signup;
       });
-      _animationController.forward(); //avança animação
+      //_animationController.forward(); //avança animação
     } else {
       setState(() {
         _authMode = AuthMode.Login;
       });
-      _animationController.reverse(); //volta animação
+      //_animationController.reverse(); //volta animação
     }
   }
 
@@ -209,16 +211,13 @@ class _AuthCardState extends State<AuthCard>
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: AnimatedBuilder(
-        animation: _heightAnimation,
-        builder: (ctx, ch) => Container(
-          //height: _authMode == AuthMode.Signup ? 320 : 260,
-          height: _heightAnimation.value.height,
-          constraints: BoxConstraints(minHeight: _heightAnimation.value.height),
-          width: deviceSize.width * 0.75,
-          padding: EdgeInsets.all(16.0),
-          child: ch,
-        ),
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 300),
+        curve: Curves.easeIn,
+        height: _authMode == AuthMode.Signup ? 320 : 260,
+        constraints: BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
+        width: deviceSize.width * 0.75,
+        padding: EdgeInsets.all(16.0),
         child: Form(
           key: _formKey,
           child: SingleChildScrollView(
@@ -296,4 +295,101 @@ class _AuthCardState extends State<AuthCard>
       ),
     );
   }
+
+  //AnimatedBuilder
+  // @override
+  // Widget build(BuildContext context) {
+  //   final deviceSize = MediaQuery.of(context).size;
+  //   return Card(
+  //     shape: RoundedRectangleBorder(
+  //       borderRadius: BorderRadius.circular(10.0),
+  //     ),
+  //     elevation: 8.0,
+  //     child: AnimatedBuilder(
+  //       animation: _heightAnimation,
+  //       builder: (ctx, ch) => Container(
+  //         //height: _authMode == AuthMode.Signup ? 320 : 260,
+  //         height: _heightAnimation.value.height,
+  //         constraints: BoxConstraints(minHeight: _heightAnimation.value.height),
+  //         width: deviceSize.width * 0.75,
+  //         padding: EdgeInsets.all(16.0),
+  //         child: ch,
+  //       ),
+  //       child: Form(
+  //         key: _formKey,
+  //         child: SingleChildScrollView(
+  //           child: Column(
+  //             children: <Widget>[
+  //               TextFormField(
+  //                 decoration: InputDecoration(labelText: 'E-Mail'),
+  //                 keyboardType: TextInputType.emailAddress,
+  //                 validator: (value) {
+  //                   if (value.isEmpty || !value.contains('@')) {
+  //                     return 'Invalid email!';
+  //                   }
+  //                   return null;
+  //                 },
+  //                 onSaved: (value) {
+  //                   _authData['email'] = value;
+  //                 },
+  //               ),
+  //               TextFormField(
+  //                 decoration: InputDecoration(labelText: 'Password'),
+  //                 obscureText: true,
+  //                 controller: _passwordController,
+  //                 validator: (value) {
+  //                   if (value.isEmpty || value.length < 5) {
+  //                     return 'Password is too short!';
+  //                   }
+  //                 },
+  //                 onSaved: (value) {
+  //                   _authData['password'] = value;
+  //                 },
+  //               ),
+  //               if (_authMode == AuthMode.Signup)
+  //                 TextFormField(
+  //                   enabled: _authMode == AuthMode.Signup,
+  //                   decoration: InputDecoration(labelText: 'Confirm Password'),
+  //                   obscureText: true,
+  //                   validator: _authMode == AuthMode.Signup
+  //                       ? (value) {
+  //                           if (value != _passwordController.text) {
+  //                             return 'Passwords do not match!';
+  //                           }
+  //                         }
+  //                       : null,
+  //                 ),
+  //               SizedBox(
+  //                 height: 20,
+  //               ),
+  //               if (_isLoading)
+  //                 CircularProgressIndicator()
+  //               else
+  //                 RaisedButton(
+  //                   child:
+  //                       Text(_authMode == AuthMode.Login ? 'LOGIN' : 'SIGN UP'),
+  //                   onPressed: _submit,
+  //                   shape: RoundedRectangleBorder(
+  //                     borderRadius: BorderRadius.circular(30),
+  //                   ),
+  //                   padding:
+  //                       EdgeInsets.symmetric(horizontal: 30.0, vertical: 8.0),
+  //                   color: Theme.of(context).primaryColor,
+  //                   textColor: Theme.of(context).primaryTextTheme.button.color,
+  //                 ),
+  //               FlatButton(
+  //                 child: Text(
+  //                     '${_authMode == AuthMode.Login ? 'SIGNUP' : 'LOGIN'} INSTEAD'),
+  //                 onPressed: _switchAuthMode,
+  //                 padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 4),
+  //                 materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+  //                 textColor: Theme.of(context).primaryColor,
+  //               ),
+  //             ],
+  //           ),
+  //         ),
+  //       ),
+  //     ),
+  //   );
+  // }
 }
